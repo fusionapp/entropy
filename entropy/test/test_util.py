7e0f1f163d580ae80a2f3def12b4fb8c8c5e2199
Tristan Seligmann <mithrandi@mithrandi.za.net>
2008-03-12 18:56:30 +0200

    Add some unit tests.

---
+++ entropy/test/test_util.py
@@ -0,0 +1,24 @@
+from twisted.trial.unittest import TestCase
+from twisted.cred.portal import IRealm
+
+from axiom.store import Store
+
+from xmantissa.ixmantissa import IOfferingTechnician
+from xmantissa.offering import getOfferings
+
+from entropy.util import getAppStore
+
+class GetAppStoreTests(TestCase):
+    def setUp(self):
+        self.siteStore = Store(self.mktemp())
+        for offering in getOfferings():
+            if offering.name == u'Entropy':
+                break
+        else:
+            raise RuntimeError(u'Could not find Entropy offering')
+
+        IOfferingTechnician(self.siteStore).installOffering(offering)
+
+    def test_getAppStore(self):
+        appStore = IRealm(self.siteStore).accountByAddress(u'Entropy', None).avatars.open()
+        self.assertIdentical(appStore, getAppStore(self.siteStore))
