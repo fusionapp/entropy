1630df266fa3f816c778d03a3c8e101cc8057582
Tristan Seligmann <mithrandi@mithrandi.za.net>
2009-03-16 04:58:29 +0200

    Drop M2Crypto use, make hashlib a hard dependency.

--- entropy/hash.py
+++ entropy/hash.py
@@ -1,47 +1,6 @@
 from entropy.errors import UnknownHashAlgorithm
 
-try:
-    from hashlib import sha256
-except ImportError:
-    sha256 = None
-
-try:
-    from M2Crypto.EVP import MessageDigest
-except ImportError:
-    MessageDigest = None
-
-
-class MessageDigestWrapper(object):
-    """
-    hashlib-like wrapper around L{MessageDigest}
-    """
-    def __init__(self, algo, data=None):
-        self.frozen = False
-        self.md = MessageDigest(algo)
-        if data is not None:
-            self.update(data)
-
-    def update(self, data):
-        if self.frozen:
-            raise RuntimeError('Cannot update() after retrieving digest')
-        self.md.update(data)
-
-    def digest(self):
-        if self.frozen:
-            return self._digest
-        else:
-            self.frozen = True
-            self._digest = self.md.digest()
-            return self._digest
-
-    def hexdigest(self):
-        return self.digest().encode('hex')
-
-
-if sha256 is None:
-    def sha256(*a, **kw):
-        return MessageDigestWrapper('sha256', *a, **kw)
-
+from hashlib import sha256
 
 _hashes = {
     u'sha256': sha256,
