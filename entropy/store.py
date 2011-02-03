7da5db4203b682a339137d5e2b91df960fdca747
Jeremy Thurgood <firxen@gmail.com>
2011-02-04 00:36:26 +0200

    Switched to objectId-based API. No idea how to make both versions available.

--- entropy/store.py
+++ entropy/store.py
@@ -230,16 +230,16 @@ class ObjectCreator(object):
     """
     implements(IResource)
 
-    def __init__(self, contentStore):
+    def __init__(self, contentStore, objectId=None):
         self.contentStore = contentStore
+        self.objectId = objectId
 
 
     # IResource
     def renderHTTP(self, ctx):
         req = IRequest(ctx)
         if req.method == 'GET':
-            req.setHeader('Content-Type', 'text/plain')
-            return 'PUT data here to create an object.'
+            return self.handleGET(req)
         elif req.method == 'PUT':
             return self.handlePUT(req)
         else:
@@ -248,13 +248,23 @@ class ObjectCreator(object):
             return 'Method not allowed'
 
 
+    def handleGET(self, req):
+        req.setHeader('Content-Type', 'text/plain')
+        return 'PUT data here to create an object.'
+
+
     def handlePUT(self, req):
         data = req.content.read()
+        if self.objectId is None:
+            contentDigest = getHash(self.contentStore.hash)(data).hexdigest()
+            self.objectId = u'%s:%s' % (self.contentStore.hash, contentDigest)
+        return self.putObject(self.objectId, data, req)
+
+
+    def putObject(self, objectId, data, req):
         contentType = unicode(
             req.getHeader('Content-Type') or 'application/octet-stream',
             'ascii')
-
-        contentDigest = getHash(self.contentStore.hash)(data).hexdigest()
 
         contentMD5 = req.getHeader('Content-MD5')
         if contentMD5 is not None:
@@ -268,7 +278,7 @@ class ObjectCreator(object):
             objectId = objectId.encode('ascii')
             return objectId
 
-        d = self.contentStore.storeObject(u'%s:%s' % (self.contentStore.hash, contentDigest), data, contentType)
+        d = self.contentStore.storeObject(objectId, data, contentType)
         return d.addCallback(_cb)
 
 
@@ -324,6 +334,61 @@ class ContentResource(Item):
         """
         if len(segments) >= 1:
             res = self.childFactory(segments[0])
+            if res is not None:
+                return res, segments[1:]
+        return NotFound
+
+
+
+class IdContentResource(Item):
+    """
+    Resource for accessing the content store.
+    """
+    implements(IResource)
+    powerupInterfaces = [IResource]
+
+    addSlash = inmemory()
+
+    contentStore = dependsOn(ContentStore)
+
+    def getObject(self, name):
+        def _notFound(f):
+            f.trap(NonexistentObject)
+            return None
+        return self.contentStore.getSiblingObject(name).addErrback(_notFound)
+
+
+    def childFactory(self, name, method):
+        """
+        Hook up children.
+
+        / is the root, nothing to see here.
+
+        /<objectId> is where existing objects are stored retrieved.
+        """
+        if name == '':
+            return self
+        elif method in ['PUT', 'POST']:
+            return ObjectCreator(self.contentStore, unicode(name, 'ascii'))
+        else:
+            return self.getObject(unicode(name, 'ascii'))
+        return None
+
+
+    # IResource
+    def renderHTTP(self, ctx):
+        """
+        Nothing to see here.
+        """
+        return 'Entropy'
+
+
+    def locateChild(self, ctx, segments):
+        """
+        Dispatch to L{childFactory}.
+        """
+        if len(segments) >= 1:
+            res = self.childFactory(segments[0], IRequest(ctx).method)
             if res is not None:
                 return res, segments[1:]
         return NotFound
--- xmantissa/plugins/entropyoff.py
+++ xmantissa/plugins/entropyoff.py
@@ -4,14 +4,14 @@ Dropin with Entropy offering.
 from xmantissa.offering import Offering
 
 from entropy.web import SimpleSiteFactory
-from entropy.store import ContentResource
+from entropy.store import ContentResource, IdContentResource
 
 
 plugin = Offering(
     name=u'Entropy',
     description=u'A persistent, immutable object store.',
     siteRequirements=[(None, SimpleSiteFactory)],
-    appPowerups=[ContentResource],
+    appPowerups=[IdContentResource],
     installablePowerups=[],
     loginInterfaces=[],
     themes=[])
