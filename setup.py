22cfb0ca4a90af5cfdac7b718b732a514c5da16e
Jonathan Jacobs <korpse@slipgate.za.net>
2011-09-19 17:28:15 +0200

    setup.py.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+setup(
+    name='Entropy',
+    version='0.0.1',
+    maintainer='Entropy developers',
+    description='A simple content-addressed immutable object store, with flexible backend support.',
+    url='https://launchpad.net/entropy-store/',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Twisted',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Archiving'],
+    packages=find_packages() + ['xmantissa.plugins'],
+    install_requires=['Twisted >= 2.5.0',
+                      'Epsilon >= 0.5.0',
+                      'Axiom >= 0.5.20',
+                      'Nevow >= 0.9.8',
+                      'txAWS >= 0.2',
+                      'Mantissa >= 0.6.1'])
