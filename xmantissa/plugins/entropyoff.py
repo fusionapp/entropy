2c99afe447ef69aac3883bcd4f8a3453412d866e
Darren Dormer <potato@dekrypt.net>
2012-07-31 16:41:49 +0200

    - Added shannon as an offering.

--- xmantissa/plugins/entropyoff.py
+++ xmantissa/plugins/entropyoff.py
@@ -6,12 +6,25 @@ from xmantissa.offering import Offering
 from entropy.web import SimpleSiteFactory
 from entropy.store import ContentResource
 
+from shannon.main import CoreResource
+from shannon.web import SimpleSiteFactory as ShannonSiteFactory
+
 
 plugin = Offering(
     name=u'Entropy',
     description=u'A persistent, immutable object store.',
     siteRequirements=[(None, SimpleSiteFactory)],
     appPowerups=[ContentResource],
+    installablePowerups=[],
+    loginInterfaces=[],
+    themes=[])
+
+
+plugin = Offering(
+    name=u'Shannon',
+    description=u'A persistent, immutable object store.',
+    siteRequirements=[(None, ShannonSiteFactory)],
+    appPowerups=[CoreResource],
     installablePowerups=[],
     loginInterfaces=[],
     themes=[])
