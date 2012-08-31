e57e0e2cf0065a025da87cc7762efbf6b7df3dad
Darren Dormer <potato@dekrypt.net>
2012-08-31 16:37:08 +0200

    -Added tests for util.py

---
+++ shannon/test/test_util.py
@@ -0,0 +1,44 @@
+import json
+from uuid import uuid1
+
+from twisted.trial.unittest import TestCase
+
+from shannon.util import (tagsToStr, tagsToDict, retrieveEncoder)
+
+
+
+class UtilTests(TestCase):
+    """
+    Tests for L{shannon.util}.
+    """
+    def test_tagsToStr(self):
+        """
+        C{tagsToStr} takes a C{dict} and returns a C{str}.
+        """
+        tags = dict(test1='test', test2='more test')
+        self.assertEqual('test1=test, test2=more test', tagsToStr(tags))
+
+        tag = dict(test='test')
+        self.assertEqual('test=test', tagsToStr(tag))
+
+
+    def test_tagsToDict(self):
+        """
+        C{tagsToDict} takes a C{str} and returns a C{dict}.
+        """
+        tags = 'test1=value1, test2=value2'
+        tagsDict = dict(test1='value1', test2='value2')
+        self.assertEqual(tagsDict, tagsToDict(tags))
+
+        tag = 'test1=value1'
+        tagDict = dict(test1='value1')
+        self.assertEqual(tagDict, tagsToDict(tag))
+
+
+    def test_retrieveEncoder(self):
+        """
+        C{retrieveEncoder} handles json.UUID objects.
+        """
+        id = uuid1()
+        result = json.dumps(id, cls=retrieveEncoder)
+        self.assertEquals('"%s"' % str(id), result)
